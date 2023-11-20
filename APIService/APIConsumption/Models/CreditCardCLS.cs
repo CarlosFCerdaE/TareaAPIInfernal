9f558ba00e8de9c3d5908a835e4fa8efed4459e4
Carlos Cerda <cfcerda@uamv.edu.ni>
2023-11-19 22:45:14 -0600

    CRUD finalizado

---
+++ APIService/APIConsumption/Models/CreditCardCLS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebCRUDapi.Models
+{
+    public class CreditCardCLS
+    {
+        public int CreditCardID { get; set; }
+        public string CardType { get; set; }
+        public string CardNumber { get; set; }
+        public int ExpMonth { get; set; }
+        public int ExpYear { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
